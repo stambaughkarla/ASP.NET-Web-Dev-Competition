94960752b898fa61bebf76b144110c78d7cc24dd
stambaughkarla <stambaughkarla@gmail.com>
2024-11-01 13:48:18 -0500

    adding annotations

--- FinalProject/Models/Review.cs
+++ FinalProject/Models/Review.cs
@@ -1,24 +1,31 @@
-﻿using System.Reflection.Metadata;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
 
 namespace FinalProject.Models
 {
     public class Review
     {
         //Primary Key for Review Class
+
         public int ReviewId { get; set; }
+
+
 
         //Navigationl Property FK for Customer ID
         public Customer Customer { get; set; }
 
         //Navigationl Property FK for Property ID
+        [Required]
         public Property Property { get; set; }
 
+        [Required]
         public int Rating { get; set; }
 
         public string ReviewText { get; set; }
 
         public string HostComments { get; set;}
 
+        [Required]
         public bool DisputeStatus { get; set; }
 
 
