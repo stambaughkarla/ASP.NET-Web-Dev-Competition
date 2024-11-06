5d9bc5b567c3e8e68b3c34665002c02c1f374ed6
stambaughkarla <stambaughkarla@gmail.com>
2024-11-05 23:53:37 -0600

    push

--- FinalProject/Models/Review.cs
+++ FinalProject/Models/Review.cs
@@ -5,32 +5,34 @@ namespace FinalProject.Models
 {
     public class Review
     {
-        //Primary Key for Review Class
-
+        // PK
+        [Key]
         public int ReviewId { get; set; }
 
-
-
-        //Navigationl Property FK for Customer ID
+        // Navigational Property - Foreign Key for Customer ID
+        [Required]
         public Customer Customer { get; set; }
 
-        //Navigationl Property FK for Property ID
+        // Navigational Property - Foreign Key for Property ID
         [Required]
         public Property Property { get; set; }
 
+        // Rating, RANGE??
         [Required]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
         public int Rating { get; set; }
 
+        // Review Text range??
         [Display(Name = "Host Comments")]
+        [StringLength(1000, ErrorMessage = "Review Text cannot exceed 1000 characters.")]
         public string ReviewText { get; set; }
 
-        public string HostComments { get; set;}
+        // Host Comments range??
+        [StringLength(500, ErrorMessage = "Host Comments cannot exceed 500 characters.")]
+        public string HostComments { get; set; }
 
+        // Dispute Status with required validation
         [Required]
         public bool DisputeStatus { get; set; }
-
-
-
-
     }
 }
