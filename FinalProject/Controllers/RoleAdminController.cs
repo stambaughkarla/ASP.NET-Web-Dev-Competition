a43e6ca4475ed3437bdfc4855692b98e39bab996
Dat Ly <datvtly@gmail.com>
2024-11-27 05:43:16 -0600

    Working Admin: Manage Users
     (TODO need Reset password)

--- FinalProject/Controllers/RoleAdminController.cs
+++ FinalProject/Controllers/RoleAdminController.cs
@@ -45,6 +45,55 @@ namespace FinalProject.Controllers
             return View(users);
         }
 
+        // GET: RoleAdmin/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RoleAdmin/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Verify age requirement
+                var age = DateTime.Today.Year - model.Birthday.Year;
+                if (model.Birthday.Date > DateTime.Today.AddYears(-age)) age--;
+
+                if (age < 18)
+                {
+                    ModelState.AddModelError("Birthday", "Administrator must be at least 18 years old.");
+                    return View(model);
+                }
+
+                var user = new AppUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    PhoneNumber = model.PhoneNumber,
+                    Address = model.Address,
+                    Birthday = model.Birthday,
+                    HireStatus = true
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    // Add to Admin role
+                    await _userManager.AddToRoleAsync(user, "Admin");
+                    return RedirectToAction(nameof(Index));
+                }
+
+                AddErrorsFromResult(result);
+            }
+
+            return View(model);
+        }
+
         // GET: RoleAdmin/Edit/5
         public async Task<IActionResult> Edit(string id)
         {
@@ -59,33 +108,54 @@ namespace FinalProject.Controllers
                 return NotFound();
             }
 
-            return View(user);
+            var model = new IndexViewModel
+            {
+                UserID = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                PhoneNumber = user.PhoneNumber,
+                Address = user.Address,
+                Birthday = user.Birthday,
+                Email = user.Email,
+                Role = (await _userManager.GetRolesAsync(user)).FirstOrDefault()
+            };
+
+            return View(model);
         }
 
         // POST: RoleAdmin/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, AppUser model)
+        public async Task<IActionResult> Edit(string id, IndexViewModel model)
         {
-            if (id != model.Id)
-            {
-                return NotFound();
-            }
-
-            var user = await _userManager.FindByIdAsync(id);
-            if (user == null)
+            if (id != model.UserID)
             {
                 return NotFound();
             }
 
             if (ModelState.IsValid)
             {
+                var user = await _userManager.FindByIdAsync(id);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+
+                // Verify age requirement
+                var age = DateTime.Today.Year - model.Birthday.Year;
+                if (model.Birthday.Date > DateTime.Today.AddYears(-age)) age--;
+
+                if (age < 18)
+                {
+                    ModelState.AddModelError("Birthday", "User must be at least 18 years old.");
+                    return View(model);
+                }
+
                 user.FirstName = model.FirstName;
                 user.LastName = model.LastName;
+                user.PhoneNumber = model.PhoneNumber;
                 user.Address = model.Address;
                 user.Birthday = model.Birthday;
-                user.PhoneNumber = model.PhoneNumber;
-                user.HireStatus = model.HireStatus;
 
                 var result = await _userManager.UpdateAsync(user);
                 if (result.Succeeded)
@@ -94,7 +164,67 @@ namespace FinalProject.Controllers
                 }
                 AddErrorsFromResult(result);
             }
-            return View(user);
+            return View(model);
+        }
+
+        // POST: RoleAdmin/Deactivate/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Deactivate(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.HireStatus = false;
+            var result = await _userManager.UpdateAsync(user);
+
+            if (!result.Succeeded)
+            {
+                AddErrorsFromResult(result);
+                return RedirectToAction(nameof(Index));
+            }
+
+            // If user is an admin, remove role
+            if (await _userManager.IsInRoleAsync(user, "Admin"))
+            {
+                await _userManager.RemoveFromRoleAsync(user, "Admin");
+            }
+
+            TempData["SuccessMessage"] = "User has been deactivated.";
+            return RedirectToAction(nameof(Index));
+        }
+
+        // POST: RoleAdmin/Activate/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Activate(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.HireStatus = true;
+            var result = await _userManager.UpdateAsync(user);
+
+            if (!result.Succeeded)
+            {
+                AddErrorsFromResult(result);
+                return RedirectToAction(nameof(Index));
+            }
+
+            // If user was previously an admin, restore role
+            if (string.Equals(user.Email.Split('@')[1], "bevobnb.com", StringComparison.OrdinalIgnoreCase))
+            {
+                await _userManager.AddToRoleAsync(user, "Admin");
+            }
+
+            TempData["SuccessMessage"] = "User has been activated.";
+            return RedirectToAction(nameof(Index));
         }
 
         // POST: RoleAdmin/ResetPassword/5
@@ -116,14 +246,9 @@ namespace FinalProject.Controllers
             string resetToken = await _userManager.GeneratePasswordResetTokenAsync(user);
             var result = await _userManager.ResetPasswordAsync(user, resetToken, newPassword);
 
-            if (result.Succeeded)
+            if (!result.Succeeded)
             {
-                return RedirectToAction(nameof(Index));
-            }
-
-            foreach (var error in result.Errors)
-            {
-                ModelState.AddModelError("", error.Description);
+                AddErrorsFromResult(result);
             }
 
             return RedirectToAction(nameof(Index));
--- FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcda4e556412ff31977cfe29511991deed0be7ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+166a4c3abdc891ce3d5aebf76246e963d39fe4c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
