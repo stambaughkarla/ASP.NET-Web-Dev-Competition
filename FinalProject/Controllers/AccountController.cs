84908141002ef6b8bc80b089a1cce1f350285e40
Dat Ly <datvtly@gmail.com>
2024-11-27 01:23:19 -0600

    Fixed naming for Account Controller

--- FinalProject/Controllers/AccountController.cs
+++ FinalProject/Controllers/AccountController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BevoBnB.Controllers
+namespace FinalProject.Controllers
 {
     [Authorize]
     public class AccountController : Controller
