29bc7da8fd015e7741858d99308cd384e15210b4
stambaughkarla <stambaughkarla@gmail.com>
2024-12-02 11:34:06 -0600

    host dahsboard

--- FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d64f1ae1a73e2863526516c34eaee6cf9a86238")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0c632f55e3aaaf3991f1369c3a5fb5af667c899")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
