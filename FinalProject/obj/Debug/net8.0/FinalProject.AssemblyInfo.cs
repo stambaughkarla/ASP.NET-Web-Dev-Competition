a8ad8421fb09d87dca3ef04610f72c19f5871df4
stambaughkarla <stambaughkarla@gmail.com>
2024-12-01 20:57:48 -0600

    adding

--- FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dc569f7f9d29eefd359ec8a5ffd6e614bbdbbba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92a5a49e75510eb2a76f9ac45d6b5508b859939a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
