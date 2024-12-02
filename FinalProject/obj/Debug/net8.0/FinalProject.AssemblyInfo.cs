0b63e4f08c17dceefe32324b22f82dddf91513ad
Dat Ly <datvtly@gmail.com>
2024-12-01 21:29:00 -0600

    Working Report

--- FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ed442bf83e5c3dc3f97f36ee4ac936641b80561")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5c128f95b9ca17b6006807785d8568292826a87")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
