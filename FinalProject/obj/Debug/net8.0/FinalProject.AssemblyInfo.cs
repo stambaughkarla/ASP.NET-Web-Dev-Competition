05ccfb55fe079543f4d32e886158b81c0a5a787e
Dat Ly <datvtly@gmail.com>
2024-12-03 11:50:10 -0600

    Fix script for Index and Search view in Property

--- FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6b8a6fa07ddb28ebbec8964e6ab329911acd825")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd14721b5b97dac38e1d0b46efe1e12bedcd764e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
