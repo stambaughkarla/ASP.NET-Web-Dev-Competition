29469c7185569414c6a54740f6b5c6101174a047
stambaughkarla <stambaughkarla@gmail.com>
2024-12-08 14:06:17 -0600

    reg fix

--- FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a8ccbc6d4f4ba2040808f5c9046dc5441507d01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adac8ebbdedc97bee4a3450faa84afa17546c458")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
